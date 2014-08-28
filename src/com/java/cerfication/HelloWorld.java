f6b3160ebd3b94efac12e60fa2e968fe183021d9
michaelmanangan <Daddy@Usernames-iMac.local>
2014-08-29 09:18:06 +1000

    Second commit testing

--- src/com/java/cerfication/HelloWorld.java
+++ src/com/java/cerfication/HelloWorld.java
@@ -3,7 +3,7 @@ package com.java.cerfication;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HI OZ");
+		System.out.println("HI Australia!");
 		
 	}
 
