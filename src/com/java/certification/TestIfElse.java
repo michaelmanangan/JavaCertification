1bfef687b66382fc63377422f1070718e80ba4f4
Michael Manangan <michael.manangan@gmail.com>
2014-10-18 20:05:50 +1100

    TestIfElse.java added

---
+++ src/com/java/certification/TestIfElse.java
@@ -0,0 +1,16 @@
+package com.java.certification;
+
+public class TestIfElse {
+
+	public static void main(String[] args) {
+		boolean b = false;
+		if (b=true) 
+		{
+			System.out.println("Value true of b is " + b); 
+		} 
+		else {
+			System.out.println("Value false of b is " + b); 
+		}
+	}
+
+}
