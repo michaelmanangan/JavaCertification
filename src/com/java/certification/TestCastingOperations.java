d8bf2ec35b5b62ec94bd54f9200b2a5b5bfd18b4
Michael Manangan <michael.manangan@gmail.com>
2014-10-19 21:40:28 +1100

    Added TestCastingOperations.java

---
+++ src/com/java/certification/TestCastingOperations.java
@@ -0,0 +1,20 @@
+package com.java.certification;
+
+public class TestCastingOperations {
+
+	public static void main(String[] args) {
+		short s = (short) 100000;
+		System.out.println(s);
+		System.out.println(Short.MAX_VALUE);
+		short a = 0;
+		a = 2;
+		a = 7 + 6;
+		a = (short) (a + 6);
+		System.out.println(a);
+		s = 1 + (int) 1;
+		System.out.println(s);
+				 
+		
+	}
+
+}
