2b45c15c7b564bf86d65ff607632cd126085f800
Michael Manangan <michael.manangan@gmail.com>
2014-10-19 21:04:14 +1100

    TestFlowControl

---
+++ src/com/java/certification/TestFlowControl.java
@@ -0,0 +1,29 @@
+package com.java.certification;
+
+public class TestFlowControl {
+	static public void main (String[] args) {
+		//String str = "Michael";
+		String str = new String("Michael");
+		
+		//switch uses the method String.equals to check equality of strings
+		switch (str) {
+			case "Michael": System.out.println("Michael True"); break;
+			default: System.out.println("Michael Default");
+		}
+
+		String[] nameList = {"Bing", "Bong", "May"};
+		outer:
+		for (String outer:nameList) 
+			for (String inner:nameList) {
+				if (inner.equals("Bong")) {
+//					break;
+					break outer;
+//					continue outer;
+				}	
+				System.out.print(inner + ":");
+			}
+		System.out.println();
+		System.out.println("Finish");
+		
+	}
+}
--- src/com/java/certification/TestStringParmInObjects.java
+++ src/com/java/certification/TestStringParmInObjects.java
@@ -11,6 +11,13 @@ public class TestStringParmInObjects {
 		s = "1";
 		
 	}
+	
+	public void TestString2 (StringBuilder s) {
+		StringBuilder s1 = new StringBuilder("Shaa!!"); 
+		s = s1;
+		//s = s.reverse();
+	}
+	
 	public static void main(String[] args) {
 		TestStringParmInObjects test = new TestStringParmInObjects("Mike");
 		String s = "Bing";
@@ -18,6 +25,9 @@ public class TestStringParmInObjects {
 		System.out.println(s);
 		test.TestString2(test.name);
 		System.out.println(test.name);
+		StringBuilder sb = new StringBuilder("Michael Jordan");
+		test.TestString2(sb);
+		System.out.println(sb);
 		
 	}
 
